0f0d1a8dd3f959694726c6c614b86c55976bbe87
nurali-code <nur3.dav.97@gmail.com>
2023-10-23 11:55:59 +0500

    second page

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
+$(document).ready(function () {
 
-$('.slider').slick({
-    arrows: true,
-    infinite: true,
-    speed: 400,
-    slidesToShow: 1,
-    swipeToSlide: true,
-    centerMode: true,
-    variableWidth: true,
-    adaptiveHeight: false,
+    $('.slider').slick({
+        arrows: true,
+        infinite: true,
+        speed: 400,
+        slidesToShow: 1,
+        swipeToSlide: true,
+        centerMode: true,
+        variableWidth: true,
+        adaptiveHeight: false,
+    });
+
+
+    var listCounter = 1;
+
+    $("ol.styled li").each(function () {
+        if (!$(this).hasClass("headline")) {
+            $(this).attr("value", listCounter++);
+        }
+    });
+
+    $('.btnPlay').on('click', function (e) {
+        $(this).fadeOut();
+        $(this).next('video')[0].play();
+        $(this).next('video').attr('controls', true);
+    })
 });
+
